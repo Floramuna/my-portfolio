565dda85930697f08383fdb4b6056b894b3eab0b
FLORAMUNA <floramuna706@gmail.com>
2025-04-04 23:19:48 +0300

    Your commit message here

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    middlewareMode: true,
-  },
-  preview: {
-    historyApiFallback: true,
+  base: '/my-portfolio/',
+  build: {
+    outDir: 'dist'
   }
 })
